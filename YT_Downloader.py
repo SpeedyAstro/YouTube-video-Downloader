4caab61b90b1d7527bf6fd2242ce51280f1ca839
Anubhav Pandey <57632430+Mr-Anubhav-pandey@users.noreply.github.com>
2022-01-27 23:49:54 +0530

    Update YT_Downloader.py

--- YT_Downloader.py
+++ YT_Downloader.py
@@ -1,33 +1,27 @@
+
+# importing the module 
 from pytube import YouTube
 
 ch = {1 : "Title" ,2 : "Thumbnail", 3 : "Download Video" , 0 : "Exit" }
 
-# def get_title(link):
-#     yt = YouTube(link)
-#     print(yt.title(link))
-# def get_thumbnail():
-#     pass
-# def get_video():
-#     pass
-
-# def main():
 
 if __name__ == "__main__":
+    # link of the video to be downloaded 
+    # paste link 
     link = "https://www.youtube.com/watch?v=yMpt1AAIRPQ"
     yt = YouTube(link)
     while True:
         print(ch)
         oc = int(input())
         if oc == 1:
-            # get_title(link)
+            # get_title
             print(yt.title)
         elif oc == 2:
-            # get_thumbnail(link)
+            # get_thumbnail
             print(yt.thumbnail_url)
         elif oc == 3:
-            # get_video(link)
+            # where to save 
             SAVE_PATH = "D:\python"
-            # mp4files = yt.filter('mp4')
             # d_video = yt.get(mp4files[-1].extension,mp4files[-1].resolution) 
             # stream = yt.streams.first()
             
@@ -36,27 +30,11 @@ if __name__ == "__main__":
                 # stream.download(SAVE_PATH)
                 yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')[-1].download(SAVE_PATH)
             except: 
-                print("Some Error!") 
-            print('Task Completed!') 
+                print("Connection Error!") 
+            print('Task Completed!')
+        elif oc == 0:
+            exit()
         else :
             print("Enter valid Choice")
 
-
-
-
-
-
-
-
-# link = "https://www.youtube.com/watch?v=EAYlckSaviI"
-# yt = YouTube(link)
-
-# print(yt.title)
-# print(yt.thumbnail_url)
-# videos = yt.streams.all()
-# vid = list(enumerate(videos))
-# for i in vid:
-#     print(i)
-# strm = int(input("enter :"))
-# # videos[strm].download()
 
